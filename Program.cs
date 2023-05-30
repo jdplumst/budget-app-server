043b6debb92862deb0a86686480c6474c649c54c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-05-30 00:05:08 -0400

    Add CORS settings

--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+//     {
+//         builder.AllowAnyOrigin()
+//                .AllowAnyMethod()
+//                .AllowAnyHeader();
+//     }));
+builder.Services.AddCors();
 
 DotNetEnv.Env.Load();
 
-var app = builder.Build();
+var app = builder.Build();// Add Cors
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors((o) => o.WithOrigins("http://localhost:3000", "https://budget-app-coral.vercel.app/"));
 
 app.UseHttpsRedirection();
 
