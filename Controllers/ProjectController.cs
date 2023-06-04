387df2d58f8c244fdb3eed4c366d021d0ae8a404
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-06-03 23:22:19 -0400

    Add Project Controller

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,36 @@
+using BudgetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace budget_app_server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProjectController : ControllerBase
+{
+
+    private readonly BudgetAppContext context;
+    public ProjectController(BudgetAppContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<List<Project>> GetAll()
+    {
+        return context.Projects.ToList(); ;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Project> GetById(int id)
+    {
+        var project = context.Projects.Find(id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return project;
+    }
+}
