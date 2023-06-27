5268d27c0b89d90d44904dc689d1a535440f3565
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-06-27 11:27:38 -0400

    Add UserController and Get User endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using BudgetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace budget_app_server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly BudgetAppContext context;
+
+    public UserController(BudgetAppContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<User> Get()
+    {
+        int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var user = context.Users.Where((u) => u.Id == userId).Select((u) => new { u.Username }).FirstOrDefault();
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+}
