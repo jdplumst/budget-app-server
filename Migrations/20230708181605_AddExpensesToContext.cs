8cfec1ddb9fea457e70ad1411cdbadc6350a24b5
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-08 14:29:34 -0400

    Add migration for adding Expenses to context

---
+++ Migrations/20230708181605_AddExpensesToContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_app_server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExpensesToContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expense_Projects_ProjectId",
+                table: "Expense");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense");
+
+            migrationBuilder.RenameTable(
+                name: "Expense",
+                newName: "Expenses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expense_ProjectId",
+                table: "Expenses",
+                newName: "IX_Expenses_ProjectId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Projects_ProjectId",
+                table: "Expenses",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Projects_ProjectId",
+                table: "Expenses");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Expenses",
+                table: "Expenses");
+
+            migrationBuilder.RenameTable(
+                name: "Expenses",
+                newName: "Expense");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Expenses_ProjectId",
+                table: "Expense",
+                newName: "IX_Expense_ProjectId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Expense",
+                table: "Expense",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expense_Projects_ProjectId",
+                table: "Expense",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
