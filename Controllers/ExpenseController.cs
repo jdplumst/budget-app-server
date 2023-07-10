83229f94fc2812916ffa1dab21b079626f0ad834
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-10 12:12:51 -0400

    Add GetAll endpoint for Expenses

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -14,6 +14,32 @@ public class ExpenseController : ControllerBase
   {
     this.context = context;
   }
+
+  [HttpGet]
+  public IActionResult GetAll(int projectId = 0)
+  {
+    int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+    if (projectId == 0)
+    {
+      return BadRequest();
+    }
+    else
+    {
+      var project = context.Projects.Find(projectId);
+      if (project == null)
+      {
+        return NotFound("Project does not exist");
+      }
+      if (project.UserId != userId)
+      {
+        return Unauthorized("You are not authorized to view this project");
+      }
+      var expenses = context.Expenses.Where(e => e.ProjectId == projectId).ToList();
+      return Ok(expenses);
+    }
+  }
+
   [HttpGet("{id}")]
   public IActionResult Get(int id)
   {
