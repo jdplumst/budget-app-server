4d276076c7a04f035df00183ac0f617c20ed3d55
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-07-24 15:44:33 -0400

    Ensure that total expenses does not go over project budget when creating expense

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -9,115 +9,121 @@ namespace budget_app_server.Controllers;
 [Route("[controller]")]
 public class ExpenseController : ControllerBase
 {
-  private readonly BudgetAppContext context;
-  public ExpenseController(BudgetAppContext context)
-  {
-    this.context = context;
-  }
+    private readonly BudgetAppContext context;
+    public ExpenseController(BudgetAppContext context)
+    {
+        this.context = context;
+    }
 
-  [HttpGet]
-  public IActionResult GetAll(int projectId = 0)
-  {
-    int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
-    string userRole = User.FindFirstValue(ClaimTypes.Role)!;
-    Role role = (Role)Enum.Parse(typeof(Role), userRole);
-    if (projectId == 0 && !role.HasFlag(Role.Admin))
+    [HttpGet]
+    public IActionResult GetAll(int projectId = 0)
     {
-      return BadRequest();
+        int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        string userRole = User.FindFirstValue(ClaimTypes.Role)!;
+        Role role = (Role)Enum.Parse(typeof(Role), userRole);
+        if (projectId == 0 && !role.HasFlag(Role.Admin))
+        {
+            return BadRequest();
+        }
+        else if (projectId == 0)
+        {
+            var expenses = context.Expenses.ToList();
+            return Ok(expenses);
+        }
+        else
+        {
+            var project = context.Projects.Find(projectId);
+            if (project == null)
+            {
+                return NotFound("Project does not exist");
+            }
+            if (project.UserId != userId)
+            {
+                return Unauthorized("You are not authorized to view this project");
+            }
+            var expenses = context.Expenses.Where(e => e.ProjectId == projectId).ToList();
+            return Ok(expenses);
+        }
     }
-    else if (projectId == 0)
-    {
-      var expenses = context.Expenses.ToList();
-      return Ok(expenses);
-    }
-    else
-    {
-      var project = context.Projects.Find(projectId);
-      if (project == null)
-      {
-        return NotFound("Project does not exist");
-      }
-      if (project.UserId != userId)
-      {
-        return Unauthorized("You are not authorized to view this project");
-      }
-      var expenses = context.Expenses.Where(e => e.ProjectId == projectId).ToList();
-      return Ok(expenses);
-    }
-  }
 
-  [HttpGet("{id}")]
-  public IActionResult Get(int id)
-  {
-    var expense = context.Expenses.Find(id);
-    if (expense == null)
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
     {
-      return NotFound("Expense not found");
+        var expense = context.Expenses.Find(id);
+        if (expense == null)
+        {
+            return NotFound("Expense not found");
+        }
+        var project = context.Projects.Find(expense.ProjectId);
+        if (project == null)
+        {
+            return NotFound("Project belonging to expense does not exist");
+        }
+        int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        if (project.UserId != userId)
+        {
+            return Unauthorized("You are not authorized to view this expense");
+        }
+        return Ok(expense);
     }
-    var project = context.Projects.Find(expense.ProjectId);
-    if (project == null)
-    {
-      return NotFound("Project belonging to expense does not exist");
-    }
-    int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
-    if (project.UserId != userId)
-    {
-      return Unauthorized("You are not authorized to view this expense");
-    }
-    return Ok(expense);
-  }
 
-  [HttpPost]
-  public IActionResult Create(Expense expense)
-  {
-    if (string.IsNullOrWhiteSpace(expense.Name))
+    [HttpPost]
+    public IActionResult Create(Expense expense)
     {
-      return BadRequest("Expense Name must be non-empty");
+        if (string.IsNullOrWhiteSpace(expense.Name))
+        {
+            return BadRequest("Expense Name must be non-empty");
+        }
+        if (expense.Name.Length > 30)
+        {
+            return BadRequest("Expense Name must be 30 characters or less");
+        }
+        if (expense.Amount <= 0)
+        {
+            return BadRequest("Amount must be greater than $0");
+        }
+        int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var project = context.Projects.Find(expense.ProjectId);
+        if (project == null)
+        {
+            return BadRequest("Project does not exist");
+        }
+        if (project.UserId != userId)
+        {
+            return Unauthorized("You are not authorized to create this expense");
+        }
+        if (!Enum.IsDefined(typeof(ExpenseType), expense.Type))
+        {
+            return BadRequest("Must give a valid Expense Type");
+        }
+        var projectExpenses = context.Expenses.Where(e => e.ProjectId == project.Id);
+        var totalExpense = projectExpenses.Sum(e => e.Amount) + expense.Amount;
+        if (project.Budget < totalExpense)
+        {
+            return BadRequest("Total expense amount cannot go over budget");
+        }
+        context.Expenses.Add(expense);
+        Console.WriteLine(expense);
+        context.SaveChanges();
+        return CreatedAtAction(nameof(Get), new { id = expense.Id }, expense);
     }
-    if (expense.Name.Length > 30)
-    {
-      return BadRequest("Expense Name must be 30 characters or less");
-    }
-    if (expense.Amount <= 0)
-    {
-      return BadRequest("Amount must be greater than $0");
-    }
-    int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
-    var project = context.Projects.Find(expense.ProjectId);
-    if (project == null)
-    {
-      return BadRequest("Project does not exist");
-    }
-    if (project.UserId != userId)
-    {
-      return Unauthorized("You are not authorized to create this expense");
-    }
-    if (!Enum.IsDefined(typeof(ExpenseType), expense.Type))
-    {
-      return BadRequest("Must give a valid Expense Type");
-    }
-    context.Expenses.Add(expense);
-    Console.WriteLine(expense);
-    context.SaveChanges();
-    return CreatedAtAction(nameof(Get), new { id = expense.Id }, expense);
-  }
 
-  [HttpDelete("{id}")]
-  public IActionResult Delete(int id)
-  {
-    int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
-    var expense = context.Expenses.Find(id);
-    if (expense == null)
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
     {
-      return NotFound("Expense does not exist");
+        int userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var expense = context.Expenses.Find(id);
+        if (expense == null)
+        {
+            return NotFound("Expense does not exist");
+        }
+        var project = context.Projects.Where(p => p.Id == expense.ProjectId).First();
+        if (project.UserId != userId)
+        {
+            return Unauthorized("You are not authorized to delete this expense");
+        }
+        context.Expenses.Remove(expense);
+        context.SaveChanges();
+        return Ok(expense);
     }
-    var project = context.Projects.Where(p => p.Id == expense.ProjectId).First();
-    if (project.UserId != userId)
-    {
-      return Unauthorized("You are not authorized to delete this expense");
-    }
-    context.Expenses.Remove(expense);
-    context.SaveChanges();
-    return Ok(expense);
-  }
 }
